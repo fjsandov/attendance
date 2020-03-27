bcafb26a5843108480159de0e0dfc8e66da2cb55
Francisco Sandoval <francisco@mtblc.co>
2020-03-27 11:58:25 -0300

    trigger deploy

--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 echo "Running release.sh"
 # RAILS_ENV=production rails db:migrate
 if [ "$APP_ENVIRONMENT" == "production" ]; then
-  echo "deploy to production not available"
+  echo "Deploy to production not available"
   exit 1
 fi
 
-echo "deploy to staging"
+echo "Deploy to staging"
